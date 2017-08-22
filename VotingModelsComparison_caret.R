b88a165564ac111f598c81b162dd10773725fb88
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-08-23 00:18:06 +0200

    created file

---
+++ VotingModelsComparison_caret.R
@@ -0,0 +1,17 @@
+#Model comparisons using CARET package
+#Which model provides the best predictions?
+#NaiveBayes,KNN, RandomForest, SVN, or GLM
+
+#libraries
+
+#import data
+
+#create trainControl to be reused by all models
+
+#generate each model
+
+#compare models by
+#sensitivity
+#specificity
+#ROC
+
