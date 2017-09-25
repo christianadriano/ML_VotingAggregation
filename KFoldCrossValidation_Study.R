79f7320ca79e894aebe6a85eeb6eb4cdd837e317
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-09-25 19:30:40 +0200

    k-fold cross-validation study effect of folds

---
+++ KFoldCrossValidation_Study.R
@@ -0,0 +1,77 @@
+#K-Fold cross-validation
+#Compare results from different values of K
+#KNN, RandomForest, GLM, or SVN
+
+#libraries
+library(caret)
+
+
+####################
+#Import data
+
+source("C://Users//chris//OneDrive//Documentos//GitHub//ML_VotingAggregation//aggregateAnswerOptionsPerQuestion.R");
+summaryTable <- runMain();
+#summaryTable <- data.frame(summaryTable);
+
+#I need to guarantee that some examples (i.e., failing methods)
+#do not dominate the training or testing sets. To do that, I need to get a 
+#close to equal proportion of examples in both sets
+
+#Scramble the dataset before extracting the training set.
+set.seed(8850);
+g<- runif((nrow(summaryTable))); #generates a random distribution
+summaryTable <- summaryTable[order(g),];
+
+##################################################
+# Create trainControl to be reused by all models #
+
+#convert columns to numeric
+summaryTable<- data.frame(summaryTable, stringsAsFactors = FALSE)
+summaryTable[,"rankingVote"] <- as.numeric(unlist(summaryTable[,"rankingVote"]));
+summaryTable[,"Yes.Count"] <- as.numeric(unlist(summaryTable[,"Yes.Count"]));
+summaryTable[,"majorityVote"] <- as.numeric(unlist(summaryTable[,"majorityVote"]));
+summaryTable[,"explanatoryVariable"] <- summaryTable[,"majorityVote"];
+summaryTable$bugCoveringLabels <- as.character(summaryTable$bugCovering);
+summaryTable$bugCoveringLabels<- replace(summaryTable$bugCoveringLabels,summaryTable$bugCoveringLabels=="FALSE", "F");
+summaryTable$bugCoveringLabels<- replace(summaryTable$bugCoveringLabels,summaryTable$bugCoveringLabels=="TRUE", "T");
+summaryTable$bugCoveringLabels<- as.factor(summaryTable$bugCoveringLabels);
+
+  
+  # Create custom indices: myFolds
+  #Guarantees that we are going to use the exact same datasets for all models
+  myFolds <- createFolds(summaryTable[,"explanatoryVariable"] , k = 10); 
+  
+  #larger K implies less bias (overfitting). However, larger K implies larger variance, i.e., 
+  #the prediction has large variation. The reason is that larger K makes each training data large and
+  #very similar.
+  #nice explanation here: https://stats.stackexchange.com/questions/27730/choice-of-k-in-k-fold-cross-validation
+  
+  # Create reusable trainControl object: myControl
+  kFoldControl <- trainControl(
+    index = myFolds, #Train with k folds and validate with one
+    classProbs = TRUE, # IMPORTANT!
+    verboseIter = TRUE, #
+    savePredictions = TRUE, #
+    summaryFunction = twoClassSummary
+  );
+  #knnModel <- train(bugCoveringLabels ~ explanatoryVariable,summaryTable, method="knn", trControl=kFoldControl);
+  #rfModel<- train(bugCoveringLabels ~ explanatoryVariable,summaryTable, method="rf", trControl=kFoldControl);
+  #bayesglmModel<- train(bugCoveringLabels ~ explanatoryVariable,summaryTable, method="bayesglm", trControl=kFoldControl);
+  svmLinearWeightsModel <- train(bugCoveringLabels ~ explanatoryVariable,summaryTable, method="svmLinearWeights", trControl=kFoldControl);
+  svmLinearWeightsModel 
+
+bugCoveringPredicted <- predict(svmLinearWeightsModel,newdata = summaryTable);
+matrixResult<- confusionMatrix(data=bugCoveringPredicted,summaryTable$bugCoveringLabels, positive="T");
+trueNegatives<- matrixResult$table[1,1];
+truePositives<- matrixResult$table[2,2];
+falseNegatives<- matrixResult$table[1,2];
+falsePositives<- matrixResult$table[2,1];
+
+accuracy <- (truePositives + trueNegatives) / (truePositives + trueNegatives + falsePositives + falseNegatives);
+trainingError <- 1-accuracy;
+precision <- truePositives / (truePositives + falsePositives);
+recall <- truePositives / (truePositives + falseNegatives);
+sensitivity <- trueNegatives / (trueNegatives + falsePositives);
+specificity <- truePositives / (truePositives + falseNegatives);
+
+
