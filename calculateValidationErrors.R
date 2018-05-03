5b139d26013858383cbfd80383c70da69b5cc9f9
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-05-03 17:34:30 +0200

    Change fucntion to compute a single line

--- calculateValidationErrors.R
+++ calculateValidationErrors.R
@@ -4,50 +4,46 @@
 
 # Calculate errors on a provided dataset (ideally a holdout one) ----------
 
-calculateValidationErrors <- function(fitModel,validationData){
- # browser()
-#  fitModel <- fitModelList[1];
-  ## Table to store the outcomes from the training and model selection and prediction
-  outcome <- matrix(ncol = 12, nrow = 40);
-  colnames(outcome)<- c("AUC","accuracy","trueNegatives","truePositives",
-                        "falseNegatives","falsePositives","precision","recall","specificity","sensitivity");
+calculateValidationErrors <- function(fitModel,modelName,validationData){
   
-  row = 0;
- # for(fitModel in fitModelList){  
-    row <- row+1;
-    bugCoveringPredicted <- predict(fitModel,newdata = validationData);
-    matrixResult<- confusionMatrix(data=bugCoveringPredicted,validationData$bugCoveringLabels, positive="T");
-    trueNegatives<- matrixResult$table[1,1];
-    truePositives<- matrixResult$table[2,2];
-    falseNegatives<- matrixResult$table[1,2];
-    falsePositives<- matrixResult$table[2,1];
-    
-    #calculate AUC
-    aucValue<- roc(response = as.numeric(validationData$bugCoveringLabels),
-                   predictor = as.numeric(bugCoveringPredicted)) %>% auc();
-    
-    aucValue<-as.numeric(aucValue);
-    
-    accuracy <- (truePositives + trueNegatives) / (truePositives + trueNegatives + falsePositives + falseNegatives);
-    trainingError <- 1-accuracy;
-    precision <- truePositives / (truePositives + falsePositives);
-    recall <- truePositives / (truePositives + falseNegatives);
-    specificity <- trueNegatives / (trueNegatives + falsePositives);
-    sensitivity <- truePositives / (truePositives + falseNegatives);
-    
-    #create output table
-    outcome[row,"AUC"] <- aucValue;
-    outcome[row,"accuracy"]<-accuracy;
-    outcome[row,"trueNegatives"]<-trueNegatives;
-    outcome[row,"truePositives"]<-truePositives;
-    outcome[row,"falseNegatives"]<-falseNegatives;
-    outcome[row,"falsePositives"]<-falsePositives;
-    outcome[row,"precision"]<-precision;
-    outcome[row,"recall"]<-recall;
-    outcome[row,"sensitivity"]<-sensitivity;
-    outcome[row,"specificity"]<-specificity;
-    
-  #}
+  outcome <- matrix(ncol = 11, nrow = 1);
+  colnames(outcome)<- c("ModelName","AUC","accuracy","trueNegatives","truePositives",
+                                   "falseNegatives","falsePositives","precision","recall","specificity","sensitivity");
+  
+  bugCoveringPredicted <- predict(fitModel,newdata = validationData);
+  
+  matrixResult<- confusionMatrix(data=bugCoveringPredicted,validationData$bugCoveringLabels, positive="T");
+  trueNegatives<- matrixResult$table[1,1];
+  truePositives<- matrixResult$table[2,2];
+  falseNegatives<- matrixResult$table[1,2];
+  falsePositives<- matrixResult$table[2,1];
+  
+  #calculate AUC
+  aucValue<- roc(response = as.numeric(validationData$bugCoveringLabels),
+                 predictor = as.numeric(bugCoveringPredicted)) %>% auc();
+  
+  aucValue<-as.numeric(aucValue);
+  
+  accuracy <- (truePositives + trueNegatives) / (truePositives + trueNegatives + falsePositives + falseNegatives);
+  trainingError <- 1-accuracy;
+  precision <- truePositives / (truePositives + falsePositives);
+  recall <- truePositives / (truePositives + falseNegatives);
+  specificity <- trueNegatives / (trueNegatives + falsePositives);
+  sensitivity <- truePositives / (truePositives + falseNegatives);
+  
+  #create output table
+  outcome[1,"ModelName"] <- modelName;
+  outcome[1,"AUC"] <- aucValue;
+  outcome[1,"accuracy"]<-accuracy;
+  outcome[1,"trueNegatives"]<-trueNegatives;
+  outcome[1,"truePositives"]<-truePositives;
+  outcome[1,"falseNegatives"]<-falseNegatives;
+  outcome[1,"falsePositives"]<-falsePositives;
+  outcome[1,"precision"]<-precision;
+  outcome[1,"recall"]<-recall;
+  outcome[1,"sensitivity"]<-sensitivity;
+  outcome[1,"specificity"]<-specificity;
+  
   return(outcome);
   
 }
